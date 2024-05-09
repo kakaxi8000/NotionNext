6bb3e17b25827f9cf80fd075a262ade83f460331
kakaxi8000 <160311712+kakaxi8000@users.noreply.github.com>
2024-05-09 20:17:10 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -15,12 +15,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'cgzyss.com',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: '轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://cgzyss.com/',
+  HEO_HERO_TITLE_1: '欢迎入群',
+  HEO_HERO_TITLE_2: '技术交流',
+  HEO_HERO_TITLE_3: 'https://t.me/kktgid',
+  HEO_HERO_TITLE_4: '搜索资源',
+  HEO_HERO_TITLE_5: '轻松搜遍电报',
+  HEO_HERO_TITLE_LINK: 'https://t.me/zyss6666',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -42,7 +42,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/kakaxi8000/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://www.cgzyss.com/', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
